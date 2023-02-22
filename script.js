353afc45e41c51c9e6c43023a6750dc7344f6f54
Murilo Melo <muriloxmelo@gmail.com>
2023-02-22 06:05:29 -0300

    navegacao-por-tab-em-class

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
 const persoMenu = document.querySelectorAll(".personagens-icon img");
 const persoContent = document.querySelectorAll("[data-tab='content'] section");
 
-if (persoMenu.length && persoContent.length) {
-  function activeBordaMenu(index) {
-    persoMenu.forEach((i) => {
-      i.classList.remove("ativo");
-    });
-    persoMenu[index].classList.toggle("ativo");
-  }
+// if (persoMenu.length && persoContent.length) {
+//   function activeBordaMenu(index) {
+//     persoMenu.forEach((i) => {
+//       i.classList.remove("ativo");
+//     });
+//     persoMenu[index].classList.toggle("ativo");
+//   }
 
-  function activeTab(index) {
-    persoContent.forEach((i) => {
-      i.classList.remove("ativo");
-    });
-    persoContent[index].classList.add("ativo");
-  }
+//   function activeTab(index) {
+//     persoContent.forEach((i) => {
+//       i.classList.remove("ativo");
+//     });
+//     persoContent[index].classList.add("ativo");
+//   }
 
-  persoMenu.forEach((i, index) => {
-    i.addEventListener("click", () => {
-      activeTab(index);
-      activeBordaMenu(index);
-    });
-  });
-}
+//   persoMenu.forEach((i, index) => {
+//     i.addEventListener("click", () => {
+//       activeTab(index);
+//       activeBordaMenu(index);
+//     });
+//   });
+// }
+
+import TabNav from "./modules.js/tabnav.js";
+
+const tabnav = new TabNav(
+  ".personagens-icon img",
+  "[data-tab='content'] section"
+);
+
+tabnav.init()
